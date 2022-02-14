4bc23e7568fb0fc8b7c112c83094918630082d4e
ryanmuhl <ryanmuhl@hotmail.com>
2022-02-13 21:02:28 -0500

    remove duplicate code

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-var generateBtn = document.querySelector("#generate");
-
-
-
-
 // Assignment code here
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var uppercaseletters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
@@ -50,12 +45,14 @@ return array2.join("");
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  
 
 }
 
